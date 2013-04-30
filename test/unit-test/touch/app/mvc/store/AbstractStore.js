230cd95a0298b14dc897c85a4ac84e60b9407818
Tim McGee <tim@webappsolution.com>
2013-04-30 09:34:58 -0400

    corrected some unit test issues for cafetownsend

---
+++ test/unit-test/touch/app/mvc/store/AbstractStore.js
@@ -0,0 +1,63 @@
+describe("FlowMVC.mvc.store.AbstractStore", function() {
+
+    //reusable scoped variable
+    var store = null;
+
+    //setup/teardown
+    beforeEach(function() {
+        //create a fresh grid for every test to avoid test pollution
+        store = Ext.create("FlowMVC.mvc.store.AbstractStore");
+    });
+
+    afterEach(function() {
+        //destroy the grid after every test so we don't pollute the environment
+        store = null;
+    });
+
+    describe("setData() method", function() {
+
+        it("should be a function", function() {
+            console.log("BMR----------------");
+            console.log(typeof store.setData);
+            expect(typeof store.setData).toEqual("function");
+        });
+
+//        it("should return a string", function() {
+//            expect(typeof FlowMVC.mvc.store.AbstractStore.getUsersUrl()).toEqual("string");
+//        });
+//
+//        it("should correctly encode the event ID", function() {
+//            var regExp = /event_id=12345/g;
+//            var eventId = 12345;
+//            var url = FlowMVC.mvc.store.AbstractStore.getUsersUrl(eventId);
+//
+//            expect(regExp.test(url)).toEqual(true);
+//        });
+
+    });
+
+//    describe("getPastEventsUrl() method", function() {
+//
+//        it("should be a function", function() {
+//            expect(typeof FlowMVC.mvc.store.AbstractStore.getPastEventsUrl).toEqual("function");
+//        });
+//
+//        it("should return a string", function() {
+//            expect(typeof FlowMVC.mvc.store.AbstractStore.getPastEventsUrl()).toEqual("string");
+//        });
+//
+//    });
+//
+//    describe("getUpcomingEventsUrl() method", function() {
+//
+//        it("should be a function", function() {
+//            expect(typeof FlowMVC.mvc.store.AbstractStore.getUpcomingEventsUrl).toEqual("function");
+//        });
+//
+//        it("should return a string", function() {
+//            expect(typeof FlowMVC.mvc.store.AbstractStore.getUpcomingEventsUrl()).toEqual("string");
+//        });
+//
+//    });
+
+});
